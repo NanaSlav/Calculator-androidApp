c18040651e7379ffae58e2fd828b1b85f6af8dae
NanaSlav <Nastya002@bk.ru>
2019-11-22 13:39:40 +0300

    Interface is ready

--- CalculatorApp/app/src/main/java/com/NanaSlav/calculator/MainActivity.java
+++ CalculatorApp/app/src/main/java/com/NanaSlav/calculator/MainActivity.java
@@ -8,25 +8,175 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    static Expression ex;
+    static {
+        ex = new Expression(" ");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
-    public void calculate(View view) {
-        Expression ex = new Expression("2+2*2");
-        double res = (double) 0;
-        try {
-            res = Calculator.calculate(ex);
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+    public void addSymbol(char symbol) {
+        char lastSymbol = ex.str.charAt(ex.str.length() - 1);
+        if (Character.isDigit(lastSymbol)) {
+            if (symbol == '(') {
+                ex.str += "*" + symbol;
+            } else {
+                if (symbol == '.') {
+                    int index = ex.str.lastIndexOf('.') + 1;
+                    while ((index < ex.str.length() - 1) && Character.isDigit(ex.str.charAt(index))) {
+                        index++;
+                    }
+                    if (index != ex.str.length() - 1) {
+                        ex.str += symbol;
+                    }
+                } else {
+                    ex.str += symbol;
+                }
+
+            }
+        } else {
+            switch (lastSymbol) {
+                case '.':
+                    if (Character.isDigit(symbol)) {
+                        ex.str += symbol;
+                    } else {
+                        if (symbol == '(') {
+                            ex.str += "0*" + symbol;
+                        } else {
+                            if (symbol != '.') {
+                                ex.str += ")" + symbol;
+                            }
+                        }
+                    }
+                    break;
+                case ')':
+                    if (Character.isDigit(symbol)) {
+                        ex.str += "*" + symbol;
+                    } else {
+                        switch (symbol) {
+                            case '.':
+                                ex.str += "*0" + symbol;
+                                break;
+                            case '(':
+                                ex.str += "*" + symbol;
+                                break;
+                            default:
+                                ex.str = ex.str.substring(0, ex.str.length() - 1);
+                                ex.str += symbol;
+                        }
+                    }
+
+                    break;
+                case '(':
+                    if (Character.isDigit(symbol)) {
+                        ex.str += symbol;
+                    } else {
+                        if (symbol == '.') {
+                            ex.str += "0" +symbol;
+                        } else {
+                            if (symbol == ')') {
+                                ex.str += symbol;
+                            }
+                        }
+                    }
+                    break;
+                default: // It will be operators (+ - * /)
+                    if (Character.isDigit(symbol)) {
+                        ex.str += symbol;
+                    } else {
+                        switch (symbol) {
+                            case '.':
+                                ex.str += "0" + symbol;
+                                break;
+                            case '(':
+                                ex.str += symbol;
+                                break;
+                            case ')':
+                                ex.str += "(";
+                            default:
+                                ex.str = ex.str.substring(0,ex.str.length() - 1);
+                                ex.str += symbol;
+
+                        }
+                    }
+
+
+
+            }
         }
-        TextView text = (TextView) findViewById(R.id.txt);
-        text.setTextSize(40);
-        text.setText(String.valueOf(res));
+        TextView text = (TextView) findViewById(R.id.display);
+        text.setText(ex.str);
     }
+
+    public void btnOne(View view) {
+        addSymbol('1');
+    }
+
+    public void btnTwo(View view) {
+        addSymbol('2');
+    }
+
+    public void btnThree(View view) {
+        addSymbol('3');
+    }
+
+    public void btnFour(View view) {
+        addSymbol('4');
+    }
+
+    public void btnFive(View view) {
+        addSymbol('5');
+    }
+
+    public void btnSix(View view) {
+        addSymbol('6');
+    }
+
+    public void btnSeven (View view) {
+        addSymbol('7');
+    }
+
+    public void btnEight (View view) {
+        addSymbol('8');
+    }
+
+    public void btnNine (View view) {
+        addSymbol('9');
+    }
+
+    public void btnZero (View view) {
+        addSymbol('0');
+    }
+
+    public void btnPlus (View view) {
+        addSymbol('+');
+    }
+
+    public void btnMinus (View view) {
+        addSymbol('-');
+    }
+
+    public void btnMult (View view) {
+        addSymbol('*');
+    }
+
+    public void btnDiv (View view) {
+        addSymbol('/');
+    }
+
+    public void btnOBracket (View view) {
+        addSymbol('(');
+    }
+
+    public void btnCBracket (View view) {
+        addSymbol(')');
+    }
+
+    public void btnDot (View view) {
+        addSymbol('.');
+    }
+
 }
